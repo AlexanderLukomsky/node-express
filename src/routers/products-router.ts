ec9c12108f3c8081a2e96b729ead64a86f19009e
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-11-23 16:22:13 +0300

    feat: validation, express-validation

--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -1,7 +1,43 @@
 import { Request, Response, Router } from "express";
+import { validationResult, body } from "express-validator";
+import { validationMiddleware } from "../middleware";
 import { productsRepository } from "../repositories";
 
 export const productsRouter = Router({});
+
+const titleValidation = body("title")
+  .trim()
+  .isLength({ min: 3, max: 10 })
+  .withMessage("title length should be from 3 to 10");
+
+productsRouter.post(
+  "/",
+  titleValidation,
+  validationMiddleware,
+  (req: Request, res: Response) => {
+    const newProduct = productsRepository.createProduct(req.body.title);
+
+    res.status(201).send(newProduct);
+  }
+);
+
+productsRouter.put(
+  "/:id",
+  titleValidation,
+  validationMiddleware,
+  (req: Request, res: Response) => {
+    const id = +req.params.id;
+    const title = req.body.title;
+    const isUpdated = productsRepository.updateProduct(id, title);
+
+    if (isUpdated) {
+      const product = productsRepository.findProductById(id);
+      res.status(200).send(product);
+    } else {
+      res.send(403);
+    }
+  }
+);
 
 productsRouter.get("/", (req: Request, res: Response) => {
   const foundProducts = productsRepository.findProducts(
@@ -28,24 +64,5 @@ productsRouter.delete("/:id", (req: Request, res: Response) => {
     res.send(204);
   } else {
     res.send(404);
-  }
-});
-
-productsRouter.post("/", (req: Request, res: Response) => {
-  const newProduct = productsRepository.createProduct(req.body.title);
-
-  res.status(201).send(newProduct);
-});
-
-productsRouter.put("/:id", (req: Request, res: Response) => {
-  const id = +req.params.id;
-  const title = req.body.title;
-  const isUpdated = productsRepository.updateProduct(id, title);
-
-  if (isUpdated) {
-    const product = productsRepository.findProductById(id);
-    res.status(200).send(product);
-  } else {
-    res.send(403);
   }
 });
